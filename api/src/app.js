08c0a1edcd7791aa12c370154400239c9d291bf7
Michael Hickson <mhickson1214@gmail.com>
2023-09-11 22:12:18 -0700

    Finished CRUD for user and inventory

--- api/src/app.js
+++ api/src/app.js
@@ -20,7 +20,105 @@ app.get('/', (req, res) => {
     res.send('Application running, Please Use an Endpoint For API discovery')
 })
 
+//GET User Profile PAGE 
+app.get('/users', (req, res) => {
+    knex('user_profile')
+        .select('*')
+        .then(data => res.status(200).json(data))
+        .catch((err) => res.status(500).json(err));
+})
+
+// POST User Profiles
+app.post('/users', async (req, res) => {
+    try {
+        const newUserProfile = req.body;
+        await knex('user_profile').insert(newUserProfile);
+        res.status(201).json('New user profile has been added.');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// PATCH User Profile by ID
+app.patch('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedUserProfile = req.body;
+        await knex('user_profile').where('id', id).update(updatedUserProfile);
+        res.json('User profile has been updated.');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// DELETE User Profile by ID
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        await knex('user_profile').where('id', id).del();
+        res.json('User profile has been deleted.');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+//GET Auction Inventory PAGE 
+app.get('/inventory', (req, res) => {
+    knex('auction_items')
+        .select('*')
+        .then(data => res.status(200).json(data))
+        .catch((err) => res.status(500).json(err));
+})
+
+// GET Auction Inventory By First Letter 
+app.get('/inventory/:letter', (req, res) => {
+    const startingLetter = req.params.letter;
+  
+    knex('auction_items')
+      .select('*')
+      .where('item_name', 'ILIKE', `${startingLetter}%`)
+      .then((data) => res.status(200).json(data))
+      .catch((err) => res.status(500).json(err));
+  })
+
+  // POST Auction Items
+app.post('/inventory', async (req, res) => {
+    try {
+        const newItem = req.body;
+        await knex('auction_items').insert(newItem);
+        res.status(201).json('New item has been added.');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// PATCH Auction Item by Name
+app.patch('/inventory/:itemName', async (req, res) => {
+    try {
+        const itemName = req.params.itemName;
+        const updatedAuctionItem = req.body;
+        await knex('auction_items').where('item_name', itemName).update(updatedAuctionItem);
+        res.json('Auction item has been updated.');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// DELETE Auction Item by Name
+app.delete('/inventory/:itemName', async (req, res) => {
+    try {
+        const itemName = req.params.itemName;
+        await knex('auction_items').where('item_name', itemName).del();
+        res.json('Auction item has been deleted.');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});  
+  
+
 //LISTEN PORT
 app.listen(port, () => {
     console.log('Knex and Express applications running successfully')
 })
+
